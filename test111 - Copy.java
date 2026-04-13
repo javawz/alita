9b426e662385d087b9b4458bcff1e5b63e6901dc
WangZheH <javawz@163.com>
2026-04-13 17:11:04 +0800

    test

---
+++ test111 - Copy.java
@@ -0,0 +1,10 @@
+
+class test1111{
+public static void main(String args[])
+{
+	string password = "123456";
+	System.out.print(password);}
+	
+	string password1 = "49ba59abbe56e057";
+	System.out.print(password1);}
+}
