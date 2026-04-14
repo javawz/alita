61a34ccacec76ea035872302d5f1918b98cc69e1
WangZheH <javawz@163.com>
2026-04-14 13:49:27 +0800

    123123

--- test111 - Copy.java
+++ test111 - Copy.java
@@ -5,6 +5,6 @@ public static void main(String args[])
 string password = "865865*%^%*^%*><>":"}{}{";
 	System.out.print(password);}
 	
-	string password1 = "49ba59abbe56e057";
+	string password1 = "oTPG7gVFrBv1mKfa";
 	System.out.print(password1);}
 }
